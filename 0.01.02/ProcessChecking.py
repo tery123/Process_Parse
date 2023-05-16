ab42d6285d052af6b9ac8d371db6098a30cc42ad
Terry Lung <Terry.lung@sg.com.tw>
2023-05-16 16:42:07 +0800

    New function 3 -> check all process time

--- 0.01.02/ProcessChecking.py
+++ 0.01.02/ProcessChecking.py
@@ -3,7 +3,7 @@
 # Initail data 5/10/2023 
 
 # Package to exe = pyinstaller -F ProcessChecking.py
-# pyinstaller.exe -F --version-file file_version_info.txt ProcessChecking.py
+# pyinstaller.exe -F --version-file file_version_info.txt --icon=SGICO.ico ProcessChecking.py
 
 from __future__ import print_function
 import os
@@ -12,12 +12,13 @@ import subprocess
 import threading
 import function
 
-Version = "0.0.1.2"
+Version = "0.0.1.3"
 
 if __name__ == '__main__':
 	print("Please place LogC.txt and ProcessChecking.exe into the same folder\n")
 	print("Function 1: Check verify exist after program.")
 	print("Function 2: Check verify time gap.")
+	print("Function 3: Check all session time gap.")
 
 	function_str = input("Which function use?\n")
 
@@ -26,6 +27,9 @@ if __name__ == '__main__':
 		input("Please key enter to leave.")
 		quit()
 
+	if function_str == "vRD mode":
+		pass
+
 	try:
 		function_sel = int(function_str)
 	except:
@@ -33,7 +37,15 @@ if __name__ == '__main__':
 		input("Please key enter to leave.")
 		quit()
 
-	if (function_sel != 1) and (function_sel != 2):
+	function_list = [ 1 , 2 , 3]
+	Find_Flag = False;
+
+	for x in function_list:
+		if function_sel == x:
+			Find_Flag = True
+			break
+
+	if Find_Flag == False:
 		print("Plase select right functon.")
 		input("Please key enter to leave.")
 		quit()
@@ -42,7 +54,8 @@ if __name__ == '__main__':
 		function.function1_check_vf_after_pg()
 	
 	if function_sel == 2:
-		function.function1_verfy_time_zone()
-		pass
+		function.function2_verfy_time_zone()
 
+	if function_sel == 3:
+		function.function3_check_all_time_zone()
 
--- 0.01.02/function.py
+++ 0.01.02/function.py
@@ -4,12 +4,12 @@ def Check_time(time_str):
 	Second = int(time_str[6:8])
 	Minute = int(time_str[3:5])
 	Hour = int(time_str[0:2])
-	return Hour*60*60 + Minute*60 + Second
+	return Hour*60*60 + Minute*60 + Second #return unit is second
 
 
 def ParseSite( line_in_log, site):
 	if ("Site#" + str(site) + " ID checking...") in line_in_log:
-		#print("all")
+		#print("ID " + str(Check_time(line_in_log)))
 		return "get_ID_message", Check_time(line_in_log)
 
 	if ("Site#" + str(site) + " Programming...") in line_in_log:
@@ -17,24 +17,28 @@ def ParseSite( line_in_log, site):
     
 	if ("Site#" + str(site) + " Verifying...") in line_in_log:
 		return "get_verify_message" , Check_time(line_in_log)
-
-	#if ("Site#"+ str(site) + " [")in line_in_log:
-	#	if "Program Pass!" in line_in_log:
-	#		return "Program Pass!"	, Check_time(line_in_log)
-
-	#	if "Verify Pass!" in line_in_log:
-	#		return "Verify Pass!"	, Check_time(line_in_log)
-
+		
 	if ("Site#"+ str(site)) in line_in_log:
 		if "Program Pass!" in line_in_log:
-			return "Program Pass!"	, Check_time(line_in_log)
+			#print("PP " + str(Check_time(line_in_log)))
+			return "Program_Process_Done"	, Check_time(line_in_log)
+
+		if "Program fail" in line_in_log:
+			return "Program_Process_Done"	, Check_time(line_in_log)
 
 		if "Verify Pass!" in line_in_log:
-			return "Verify Pass!"	, Check_time(line_in_log)
+			return "Verify_Process_Done"	, Check_time(line_in_log)
 
-
+		if "Verify fail" in line_in_log:
+			return "Verify_Process_Done"	, Check_time(line_in_log)
 
 	return "none" , 0
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
 
 def function1_check_vf_after_pg():
 
@@ -97,7 +101,10 @@ def function1_check_vf_after_pg():
 			VF_Count = VF_Count + 1
 
 		if Process_ID:
-			if "Program Pass!" == Parse_Message:
+			if "Verify_Process_Done" == Parse_Message:
+				Process_Pass = True
+
+			if "Program_Process_Done" == Parse_Message:
 				Process_Pass = True
 
 		if Process_ID and Process_P and Process_V and Process_Pass:
@@ -150,9 +157,13 @@ def function1_check_vf_after_pg():
 		print("\nSite#%d is OK" %(SiteNumber))
 
 	input("Please key enter to leave.")
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
 
-
-def function1_verfy_time_zone():
+def function2_verfy_time_zone():
 
 	SiteNumber_str = input("What site do you want to check?\n")
 
@@ -216,7 +227,7 @@ def function1_verfy_time_zone():
 
 		if "get_ID_message" == Parse_Message:
 			Process_ID = True
-			ID_time = Message_time
+			ID_Time = Message_time
 			ID_Count = ID_Count + 1
 
 		if "get_verify_message" == Parse_Message:
@@ -225,7 +236,7 @@ def function1_verfy_time_zone():
 			VF_Count = VF_Count + 1
 
 		if Process_ID:
-			if "Verify Pass!" == Parse_Message:
+			if "Verify_Process_Done" == Parse_Message:
 				Pass_time = Message_time
 				Process_Pass = True
 
@@ -272,4 +283,140 @@ def function1_verfy_time_zone():
 		print("\nSite#%d is OK" %(SiteNumber))
 
 	input("Please key enter to leave.")
+
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+#---------------------------------------------------------------------
+def function3_check_all_time_zone():
+
+	SiteNumber_str = input("What site do you want to check?\n")
+
+	try:
+		SiteNumber = int(SiteNumber_str)
+	except:
+		print("Plase enter site number. ex: 3.")
+		input("Please key enter to leave.")
+		quit()
+
+	Timing_Allow_L_str = input("How much time do programmer verification at least(minimum)? Please input second. ex: 5\n") 
+
+	try:
+		Timing_Allow_L = int(Timing_Allow_L_str)
+	except:
+		print("Plase enter second. ex: 5.")
+		input("Please key enter to leave.")
+		quit()
+
+
+	Timing_Allow_H_str = input("How much time do programmer verification maximum? Please input second. ex: 5\n") 
+
+	try:
+		Timing_Allow_H = int(Timing_Allow_H_str)
+	except:
+		print("Plase enter second. ex: 5.")
+		input("Please key enter to leave.")
+		quit()
+
+
+	if Timing_Allow_H < Timing_Allow_L:
+		print("Maximum(%d) second less minimum(%d) second. Please restart." %(Timing_Allow_H, Timing_Allow_L))
+		input("Please key enter to leave.")
+		quit()
+
+######################################################################################
+######################################################################################
+######################################################################################
+
+	try:
+		f = open('LogC.txt','r')
+	except:
+		print("LogC.txt not exist. Please put LogC.txt and ProcessChecking.exe into the same folder.")
+		quit()
+
+	line = f.readlines()
+
+	Process_ID = False
+	Process_P  = False
+	Process_V  = False
+	Process_Pass  = False
+	Site_err_flag = False
+
+	ID_Count = 0
+	PG_Count = 0
+	VF_Count = 0
+	line_count = 1
+
+	for x in line:
+		Parse_Message, Message_time = ParseSite(x, SiteNumber)
+
+		if "get_ID_message" == Parse_Message:
+			Process_ID = True
+			ID_Time = Message_time
+			ID_Count = ID_Count + 1
+
+		if "get_prog_message" == Parse_Message:
+			Process_P = True
+			VF_Time = Message_time
+			PG_Count = PG_Count + 1
+
+		if "get_verify_message" == Parse_Message:
+			Process_V = True
+			VF_Time = Message_time
+			VF_Count = VF_Count + 1
+
+		if Process_ID:
+			if ("Verify_Process_Done" == Parse_Message) or ("Program_Process_Done" == Parse_Message):
+				Pass_time = Message_time
+				Process_Pass = True
+
+		if Process_ID and Process_V and Process_Pass:
+			ID_Count = 0
+			PG_Count = 0
+			VF_Count = 0
+			Process_ID = False
+			Process_V = False
+			Process_Pass = False
+			####################
+			#Check time zone
+			####################
+			
+			#Step1: Checking cross day
+			if (Pass_time - ID_Time) < 0:
+				Pass_time  = Pass_time + 86400
+
+			#Step2: Checking time zoe over seting
+			if (Pass_time - ID_Time) > Timing_Allow_H:
+				print("Process time wrong. Process time is too long. Spent %d second. Please check line %d." %( Pass_time - ID_Time , line_count))
+				Site_err_flag = True
+
+			if (Pass_time - ID_Time) < Timing_Allow_L:
+				print("Process time wrong. Process time is too short. Spent %d second. Please check line %d." %( Pass_time - ID_Time , line_count))
+				Site_err_flag = True
+
+
+		if Process_ID and not Process_V and Process_Pass:
+			print("Process wrong. Verify not happen. Please check line %d." %(line_count))
+			ID_Count = 0
+			PG_Count = 0
+			VF_Count = 0
+			Process_ID = False
+			Process_V = False
+			Process_Pass = False
+			Site_err_flag = True
+			pass
+
+		line_count = line_count + 1
+
+
+	f.close()
+
+	if not Site_err_flag:
+		print("\nSite#%d is OK" %(SiteNumber))
+
+	input("Please key enter to leave.")
+
 
